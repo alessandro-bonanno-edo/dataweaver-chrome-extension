ea8b7ef9a401426fd7cb5f85d297d10bdeff3334
Alessandro Bonanno <alessandro.bonanno@edreamsodigeo.com>
2025-04-18 16:57:23 +0200

    feat: add chrome extension adapter, fix manifest to work as dev tool panel

---
+++ static/devtools.js
@@ -0,0 +1,3 @@
+chrome.devtools.panels.create('DataWeaver', null, 'index.html', (panel) => {
+	console.info('DataWeaver panel created', panel);
+});
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from 'sveltekit-adapter-chrome-extension';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		appDir: 'app'
+	}
 };
 
 export default config;
