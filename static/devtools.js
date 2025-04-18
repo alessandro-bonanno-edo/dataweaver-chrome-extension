5fcd345b877a97e0e9b4ebb145048d4441552540
Alessandro Bonanno <alessandro.bonanno@edreamsodigeo.com>
2025-04-18 17:21:58 +0200

    refactor: remove callback from panel creation

--- static/devtools.js
+++ static/devtools.js
@@ -1,3 +1,1 @@
-chrome.devtools.panels.create('DataWeaver', null, 'index.html', (panel) => {
-	console.info('DataWeaver panel created', panel);
-});
+chrome.devtools.panels.create('DataWeaver', null, 'index.html');
